189ededff7ea113ff325359e5c258c899a53e9eb
syu5411723 <71764394+syu5411723@users.noreply.github.com>
2020-10-17 15:41:35 +0900

    first commit

---
+++ myapp/context_processors.py
@@ -0,0 +1,11 @@
+from .models import Category
+
+
+def all_category(request):
+    category_list = Category.objects.all()
+
+    params = {
+        'category_list': category_list,
+    }
+
+    return params
---
+++ myproject/local_settings.py
@@ -0,0 +1,21 @@
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-98-@r2fh&1bgezq5%c)31ujjn=t=x)(baq)13y=f_tk5_h5g%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
