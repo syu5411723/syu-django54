7d6e59fe5b169c3147d5ef559e831e2a38e26af5
syu5411723 <71764394+syu5411723@users.noreply.github.com>
2020-10-18 06:00:28 +0900

    heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,6 +120,12 @@ LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
 # S3のための記述
+AWS_STORAGE_BUCKET_NAME = 'django-syu54'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
+}
+
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
